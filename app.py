9f0b2d4b3ba2900ba17b4ff9eb7ea2f96bc146da
Jakariya Khan <jakariya012006@gmail.com>
2025-10-02 14:16:41 +0530

    Intial Stage

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv("startup_cleaned.csv")
+
+def selected_investor_details(investor):
+    st.title(investor)
+    #Loading Recent Investings
+    last5=df[df['Investors Name'].str.contains(investor)].head()[['Date','Stratup','Vertical','City  Location','Round','Amount']]
+    st.subheader("Most Recent Investments Details(Top 5)")
+    st.dataframe(last5)
+
+
+st.sidebar.title("Startup Funding Analysis")
+
+option=st.sidebar.selectbox("Select one",["Overall Analysis","Startup","Investor"])
+
+if option=="Overall Analysis":
+    st.title("Overall Analysis")
+elif option=="Startup":
+    st.sidebar.selectbox("Select Startup",sorted(df["Stratup"].unique().tolist()))
+    btn1=st.sidebar.button("Find Startup Details")
+    st.title("Startup Analysis")
+
+else:
+    selected_investor=st.sidebar.selectbox("Select Investor",sorted(set(df["Investors Name"].str.split(',').sum())))
+    btn2=st.sidebar.button("Find Investor Details")
+    if btn2:
+        selected_investor_details(selected_investor)
+    # st.title("Investor Analysis")
